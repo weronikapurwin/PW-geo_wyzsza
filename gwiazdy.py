9f53254be3619c62c03d3dd48110acff77d08c8a
Weronika Purwin <92334838+weronikapurwin@users.noreply.github.com>
2021-12-13 23:41:40 +0100

    Update gwiazdy.py

--- gwiazdy.py
+++ gwiazdy.py
@@ -16,16 +16,28 @@ def gregorian_to_julian(y, m, d, h):
 
 def GMST(y, m, d, h):
     T = (gregorian_to_julian(y, m, d, h) - 2451545) / 36525
-    g = 280.46061837 + 360.98564736629 * (gregorian_to_julian(y, m, d, h) - 2451545.0) + 0.000387933 * T ** 3 / 38710000
+    g = 280.46061837 + 360.98564736629 * (gregorian_to_julian(y, m, d, h) - 2451545.0) + 0.000387933 * T ** 2 - T ** 3 \
+        / 38710000
     g = g % 360
     return g
 
-def kat_godzinny(y, m, d, h,lam):
-    jd = gregorian_to_julian(y, m, d, 0)
-    g = 1 #tu zaimplementuj
+
+def kat_godzinny(y, m, d, h, lam, alfa):
+    g = GMST(y, m, d, 0)
     UT1 = h * 1.002737909350795
     S = UT1 * 15 + lam + g
+    t = S - alfa * 15
+    return t
 
+def odleg_zenit(fi, dek, t):
+    z = np.sin(fi) * np.sin(dek) + np.cos(fi) * np.cos(dek) * np.cos(t)
+    return z
+
+def azymut(fi, dek, t): #tuuu dorobić
+    tag_A = (-np.cos(dek) * np.sin(t))/(np.cos(fi) * np.sin(dek) - np.sin(fi) * np.sin(dek) * np.cos(t))
+    return tag_A
 
 if __name__ == "__main__":
     print(gregorian_to_julian(1600, 1, 1, 0))
+
+    #t = kat_godzinny(y, m, d, h, lam, alfa)
