3bb761ea76119f42a8aee519611ee49f254eda89
Weronika Purwin <92334838+weronikapurwin@users.noreply.github.com>
2021-12-14 17:07:29 +0100

    Update gwiazdy.py

--- gwiazdy.py
+++ gwiazdy.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 # Gwiazda z gwiazdozbioru Byka - Elnath
 # Wawa / Nairobi (stolica Kenii) / Puerto Montt (miasto w Chile)
 
@@ -29,15 +28,36 @@ def kat_godzinny(y, m, d, h, lam, alfa):
     t = S - alfa * 15
     return t
 
-def odleg_zenit(fi, dek, t):
-    z = np.sin(fi) * np.sin(dek) + np.cos(fi) * np.cos(dek) * np.cos(t)
+def odleg_zenit(rek, dek, t):
+    z = np.sin(rek) * np.sin(dek) + np.cos(rek) * np.cos(dek) * np.cos(t)
     return z
 
-def azymut(fi, dek, t): #tuuu dorobić
-    tag_A = (-np.cos(dek) * np.sin(t))/(np.cos(fi) * np.sin(dek) - np.sin(fi) * np.sin(dek) * np.cos(t))
+def azymut(rek, dek, t): #tuuu dorobić
+    tag_A = (-np.cos(dek) * np.sin(t))/(np.cos(rek) * np.sin(dek) - np.sin(rek) * np.sin(dek) * np.cos(t))
     return tag_A
 
-if __name__ == "__main__":
-    print(gregorian_to_julian(1600, 1, 1, 0))
+def godzina_na_dzies(h, m, s):
+    return h+ m/60 +s/3600
 
-    #t = kat_godzinny(y, m, d, h, lam, alfa)
+if __name__ == "__main__":
+    # współrzedne miejsc
+    Wawa = [52.24920, 21.00030]
+    Nairobi = [-1.28333, 36.81667]
+    Puerto_Montt = [-41.4693, -72.94237]
+
+    godz = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+
+    # Rektascensja przeliczona już z godzin na stopnie
+    rek = [81.92083, 81.92083, 81.92083, 81.92083, 81.92083, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087,
+           81.92087, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087,
+           81.92087, 81.92087, 81.92087, 81.92087, 81.92092]
+
+    dek = [28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483,
+           28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483,
+           28.62483, 28.62483, 28.62483, 28.62483, 28.62483]
+
+    print(15 * godzina_na_dzies(5, 27, 41.02), godzina_na_dzies(28, 37, 29.4))
+    print(len(rek))
+    print(len(dek))
+    t_wawa = kat_godzinny(2021, 11, 30, 0, Wawa[1], rek) #?
+    print(t_wawa)
