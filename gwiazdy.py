5e5d749336bf0662c99df7a898c50d4a90766a57
Weronika Purwin <92334838+weronikapurwin@users.noreply.github.com>
2021-12-14 21:08:03 +0100

    Update gwiazdy.py

--- gwiazdy.py
+++ gwiazdy.py
@@ -25,19 +25,27 @@ def kat_godzinny(y, m, d, h, lam, alfa):
     g = GMST(y, m, d, 0)
     UT1 = h * 1.002737909350795
     S = UT1 * 15 + lam + g
-    t = S - alfa * 15
+    for value in alfa:
+        value *= 15
+    t = S - alfa
     return t
 
-def odleg_zenit(rek, dek, t):
-    z = np.sin(rek) * np.sin(dek) + np.cos(rek) * np.cos(dek) * np.cos(t)
+def odleg_zenit(fi, dek, t):
+    fi = np.deg2rad(fi)
+    dek = np.deg2rad(dek)
+    t = np.deg2rad(t)
+    z = np.sin(fi) * np.sin(dek) + np.cos(fi) * np.cos(dek) * np.cos(t)
     return z
 
-def azymut(rek, dek, t): #tuuu dorobić
-    tag_A = (-np.cos(dek) * np.sin(t))/(np.cos(rek) * np.sin(dek) - np.sin(rek) * np.sin(dek) * np.cos(t))
+def azymut(fi, dek, t): #tuuu dorobić
+    fi = np.deg2rad(fi)
+    dek = np.deg2rad(dek)
+    t = np.deg2rad(t)
+    tag_A = (-np.cos(dek) * np.sin(t))/(np.cos(fi) * np.sin(dek) - np.sin(fi) * np.cos(dek) * np.cos(t))
     return tag_A
 
 def godzina_na_dzies(h, m, s):
-    return h+ m/60 +s/3600
+    return h + m/60 + s / 3600
 
 if __name__ == "__main__":
     # współrzedne miejsc
@@ -45,8 +53,8 @@ if __name__ == "__main__":
     Nairobi = [-1.28333, 36.81667]
     Puerto_Montt = [-41.4693, -72.94237]
 
-    godz = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-
+    godz = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+    godz = np.asarray(godz)
     # Rektascensja przeliczona już z godzin na stopnie
     rek = [81.92083, 81.92083, 81.92083, 81.92083, 81.92083, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087,
            81.92087, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087, 81.92087,
@@ -56,8 +64,12 @@ if __name__ == "__main__":
            28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483, 28.62483,
            28.62483, 28.62483, 28.62483, 28.62483, 28.62483]
 
-    print(15 * godzina_na_dzies(5, 27, 41.02), godzina_na_dzies(28, 37, 29.4))
-    print(len(rek))
-    print(len(dek))
-    t_wawa = kat_godzinny(2021, 11, 30, 0, Wawa[1], rek) #?
-    print(t_wawa)
+    print(15 * godzina_na_dzies(0, 32, 0))
+    t_wawa = kat_godzinny(2021, 11, 30, godz, Wawa[1], rek)
+    t_nairobi = kat_godzinny(2021, 11, 30, godz, Nairobi[1], rek)
+    t_puerto_montt = kat_godzinny(2021, 11, 30, godz, Puerto_Montt[1], rek)
+
+    i = 0
+    while i < 25:
+        print("kąt godzinny dla Warszawy:", t_wawa[i],"godzina:", godz[i],)
+        i = i+1
